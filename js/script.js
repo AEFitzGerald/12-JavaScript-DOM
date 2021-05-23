4dbc536e1ed36d5779e030e8153a5497796af282
Anna FitzGerald <46199551+AEFitzGerald@users.noreply.github.com>
2021-05-22 19:39:43 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var mainTitle = document.querySelector("h1");
+var body = document.querySelector("body");
+
+mainTitle.innerText = "Yay, JavaScript!";
+body.style.backgroundColor = "#FEF2F1";
+
+console.log(mainTitle);
